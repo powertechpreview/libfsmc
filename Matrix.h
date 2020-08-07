de42aaeeafa69fd1ada7ed21ab9eb47693c23821
Everton Constantino <everton.constantino@ibm.com>
2020-08-07 15:29:27 -0300

    Adding SMatrix.

--- Matrix.h
+++ Matrix.h
@@ -11,14 +11,19 @@ public:
         m_data = new Scalar[rows*cols];
     }
 
-    Matrix(Index rows, Index cols, Scalar x0) : m_rows(rows), m_cols(cols)
+    Matrix(Index rows, Index cols, const Scalar& x0) : m_rows(rows), m_cols(cols)
     {
         m_data = new Scalar[rows*cols];
-        for(Index i = 0; i < rows; i++)
+        setVal(x0);
+    }
+
+    void setVal(const Scalar& v)
+    {
+        for(int i = 0; i < m_rows; i++)
         {
-            for(Index j = 0; j < cols; j++)
+            for(int j = 0; j < m_cols; j++)
             {
-                (*this)(i,j) = x0;
+                operator()(i,j) = v;
             }
         }
     }
@@ -34,7 +39,7 @@ public:
         return m_data[StorageOrder == RowMajor ? i*m_cols + j : j*m_rows + i];
     }
 
-    Matrix<Scalar> operator*(const Matrix& rhs)
+    Matrix<Scalar> operator*(Matrix& rhs)
     {
         Matrix<Scalar, Index, StorageOrder> res(m_rows, rhs.m_cols);
         for(Index i = 0; i < m_rows; i++)
@@ -43,7 +48,7 @@ public:
             {
                 for(Index k = 0; k < m_cols; k++)
                 {
-                    res(i,j) += ((*this))(i,k)*rhs(k,j);
+                    res(i,j) += operator()(i,k)*rhs(k,j);
                 }
             }
         }
@@ -57,6 +62,70 @@ public:
             for(Index j = 0; j < m.m_cols; j++)
             {
                 out << m(i,j) << " ";
+            }
+            out << std::endl;
+        }
+        return out;
+    }
+};
+
+/* Small matrix */
+/**** FIXME operator() const problem *****/
+template<typename Scalar, int rows, int cols, int StorageOrder = RowMajor>
+class SMatrix
+{
+public:
+    const int m_rows, m_cols;
+    Scalar m_data[rows*cols];
+
+    SMatrix() : m_rows(rows), m_cols(cols) {}
+
+    SMatrix(const Scalar& x0) : m_rows(rows), m_cols(cols)
+    {
+        setVal<rows, cols>(x0);
+    }
+
+    template<int r, int c>
+    void setVal(const Scalar& v)
+    {
+        for(int i = 0; i < r; i++)
+        {
+            for(int j = 0; j < c; j++)
+            {
+                m_data[StorageOrder == RowMajor ? i*cols + j : j*rows + i] = v;
+            }
+        }
+    }
+
+    inline Scalar& operator()(int i, int j) const
+    {
+        return m_data[StorageOrder == RowMajor ? i*cols + j : j*rows + i];
+    }
+
+    template<int mdepth, int mcols>
+    SMatrix<Scalar, rows, mdepth> mul(const SMatrix<Scalar, mdepth, mcols>& rhs)
+    {
+        SMatrix<Scalar, rows, mdepth> res;
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < mcols; j++)
+            {
+                for(int k = 0; k < mdepth; k++)
+                {
+                    res.m_data[StorageOrder == RowMajor ? i*mdepth + j : j*rows + i] += m_data[StorageOrder == RowMajor ? i*mdepth + k : k*rows + i]*rhs.m_data[StorageOrder == RowMajor ? k*mcols + j : j*mdepth + k];
+                }
+            }
+        }
+        return res;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const SMatrix& m)
+    {
+        for(int i = 0; i < rows;i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                out << m.m_data[StorageOrder == RowMajor ? i*cols + j : j*rows + i] << " ";
             }
             out << std::endl;
         }
--- gemm.cpp
+++ gemm.cpp
@@ -3,17 +3,26 @@
 
 int main(int argc, char* argv[])
 {
-    int s = 4;
-    Matrix<double> m1(s,s,0), m2(s,s,0);
+    const int m = 4, k = 2, n = 8;
+    Matrix<double> m1(m,k,0), m2(k,n,0);
+    SMatrix<double, m, m> sm1(1), sm2(1);
 
-    for(int i = 0; i < s; i++)
+    for(int i = 0; i < m; i++)
     {
-        for(int j = 0; j < s; j++)
+        for(int j = 0; j < k; j++)
         {
             m1(i,j) = 1;
+        }
+    }
+
+    for(int i = 0; i < k; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             m2(i,j) = 1;
         }
     }
     std::cout << "Res\n" << m1*m2 << "\n\n";
+    std::cout << "SRes\n" << sm1.mul<m,m>(sm2) << "\n\n";
     return 0;
 }
